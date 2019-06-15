abb5322c28eb1174dcbf44452ba43e51452354e0
nicholas toby <cheesasaurus@gmail.com>
2019-06-14 19:55:19 -0500

    test rollup

---
+++ test-es6modules/dist/test.js
@@ -0,0 +1,7 @@
+import { sayHello } from '../../../twcheese/test-es6modules/modules/Enter.js';
+import { sayAdios } from '../../../twcheese/test-es6modules/modules/Exit.js';
+
+TwCheese.actions.doSomething = function() {
+    sayHello();
+    sayAdios();
+};
---
+++ test-es6modules/rollup.config.js
@@ -0,0 +1,9 @@
+export default [
+    {
+        input: 'test.js',
+        output: {
+            file: 'dist/test.js',
+            format: 'esm'
+        }
+    }
+];

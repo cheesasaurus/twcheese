2375301ffb7e3a975aedf43e3eb236efdc97b737
nicholas toby <cheesasaurus@gmail.com>
2019-06-18 10:12:39 -0500

    fix dist launcher cache mechanism

--- build/templates/launch-dist.js
+++ build/templates/launch-dist.js
@@ -1,1 +1,6 @@
-$('<script>').attr('src','___DIST_URL___?___DIST_HASH___').appendTo('head');
+// Don't use jQuery to append the script element! for some reason it adds its own cachebusting string to the src every time.
+// $.getScript is supposed to do this. but pretty sure just appending a script isn't supposed to do that.
+// maybe cause its an old version of jquery?
+// maybe the game monkey patches it?
+// who knows. but using HtmlElement.appendChild works as expected
+$('head')[0].appendChild($('<script>').attr('src', '___DIST_URL___?___DIST_HASH___')[0]);

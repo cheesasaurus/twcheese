627610b019eb5057ca3e15e6f4311b83bad730af
Nicholas Toby <cheesasaurus@gmail.com>
2019-06-20 05:00:02 -0500

    put quickbar links in markdown files for better viewing

--- build/tasks/buildQuickbarLinks.js
+++ build/tasks/buildQuickbarLinks.js
@@ -6,12 +6,12 @@ const rename = require('gulp-rename');
 const fs = require('fs');
 const config = require('../tasks.config.js');
 
-let templateQuickbarLinks = fs.readFileSync('build/templates/quickbar-links', 'utf8');
+let templateQuickbarLinks = fs.readFileSync('build/templates/quickbar-links.md', 'utf8');
 
 gulp.task('buildQuickbarLinks', function() {
     return (src('launch/*.js'))
         .pipe(replaceContent(templateQuickbarLinks))
         .pipe(interpolate(config.interpolate))
-        .pipe(rename((path) => path.extname = ""))
+        .pipe(rename((path) => path.extname = ".md"))
         .pipe(dest('quickbar/'));
 });
--- build/templates/quickbar-links.md
+++ build/templates/quickbar-links.md
@@ -1,17 +1,16 @@
-===== use distributable =======================================================
+### use distributable
 
-
+```javascript
 javascript:
 (window.TwCheese && TwCheese.tryUseTool('___TOOL_ID___'))
 || $.ajax('___HOSTING_ROOT___/launch/___FILE___?'
 +~~((new Date())/3e5),{cache:1})
+```
 
+### use es modules (recommended for development)
 
-
-===== use es modules (recommended for development) ================================
-
-
+```javascript
 javascript:
 (window.TwCheese && TwCheese.tryUseTool('___TOOL_ID___'))
 || $.getScript('___HOSTING_ROOT___/launch/esm/___FILE___')
-
+```

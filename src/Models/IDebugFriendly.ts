4f0c60699290d01d77938a4ea756471698a8f5e9
Nicholas Toby <cheesasaurus@gmail.com>
2019-06-22 11:56:54 -0500

    interface for objects intended to be displayed to players in a debugger

---
+++ src/Models/IDebugFriendly.ts
@@ -0,0 +1,11 @@
+// twcheese isn't using typescript.
+//
+// This file is just for reference,
+// to help you make things look/swim/quack like a duck.
+
+
+interface IDebugFriendly {
+    valueOf(); // Object.prototype.valueOf
+    toDebugString(): string;
+    imageSrc(): string;
+}

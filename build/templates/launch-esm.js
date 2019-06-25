80046f1c2bfc04b3f9d81f63298737c5b9312efc
Nicholas Toby <cheesasaurus@gmail.com>
2019-06-25 00:21:17 -0500

    esm launcher: fix race condition where sidebar was last used tool

--- build/templates/launch-esm.js
+++ build/templates/launch-esm.js
@@ -1,15 +1,17 @@
-(function() {
+(async function() {
     let toolId = '___TOOL_ID___';
 
     ___TWCHEESE___
 
     if (!TwCheese.hasTool('Sidebar')) {
-        TwCheese.loadTool('Sidebar', () => TwCheese.useTool('Sidebar'));
+        await TwCheese.loadTool('Sidebar');
+        TwCheese.useTool('Sidebar');
     }
     
     if (TwCheese.hasTool(toolId)) {
         TwCheese.useTool(toolId);
     } else {
-        TwCheese.loadTool(toolId, () => TwCheese.useTool(toolId) );
+        await TwCheese.loadTool(toolId);
+        TwCheese.useTool(toolId);
     }
 })();

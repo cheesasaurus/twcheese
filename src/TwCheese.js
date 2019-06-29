6f2f0bd53b94ed5ea0a5c73b694997278598d554
Nicholas Toby <cheesasaurus@gmail.com>
2019-06-29 12:20:20 -0500

    clean up TwCheese.js

--- src/TwCheese.js
+++ src/TwCheese.js
@@ -1,63 +1,82 @@
+
+function handleJqXhrError(reject) {
+    return function(jqxhr) {
+        reject(new Error(`[${jqxhr.status} ${jqxhr.statusText}] ${this.type} ${this.url}`));
+    }
+}
+
+
 window.TwCheese = {
     ROOT: '___HOSTING_ROOT___',
     version: '___VERSION___',
-    actions: {},
     tools: {},
     lastToolUsedId: null,
 
-    loadVendorLibs: async function() {
-        return new Promise((resolve) => {
-            $.ajax(`${this.ROOT}/dist/vendor.js`, { complete: resolve });
-        });
-    },
-    loadVendorLibsMinified: async function(cacheBuster) {
-        return new Promise((resolve) => {
-            $.ajax(`${this.ROOT}/dist/vendor.min.js?${cacheBuster}`, {
-                cache: true,
-                complete: resolve
+    async loadVendorLibs() {
+        return new Promise((resolve, reject) => {
+            $.ajax(`${this.ROOT}/dist/vendor.js`, {
+                complete: resolve,
+                error: handleJqXhrError(reject)
             });
         });
     },
-    loadTool: async function(toolId) {
+
+    async loadVendorLibsMinified(cacheBuster) {
+        return new Promise((resolve, reject) => {
+            $.ajax(`${this.ROOT}/dist/vendor.min.js?${cacheBuster}`, {
+                cache: true,
+                complete: resolve,
+                error: handleJqXhrError(reject)
+            });
+        });
+    },
+
+    async loadTool(toolId) {
         return new Promise((resolve) => {
             let module = document.createElement('script');
             module.type = 'module';
-            module.onload = () => {
-                resolve();
-            }    
+            module.onload = resolve;
             module.src = `${this.ROOT}/src/ToolSetup/${toolId}.js`;
             document.head.appendChild(module);
         });
     },
-    loadToolCompiled: async function(toolId, cacheBuster) {
-        return new Promise((resolve) => {
+
+    async loadToolCompiled(toolId, cacheBuster) {
+        return new Promise((resolve, reject) => {
             $.ajax(`${this.ROOT}/dist/tool/setup-only/${toolId}.min.js?${cacheBuster}`, {
                 cache: true,
-                complete: resolve
+                complete: resolve,
+                error: handleJqXhrError(reject)
             });
         });         
     },
-    hasTool: function(toolId) {
+
+    hasTool(toolId) {
         return typeof this.tools[toolId] !== 'undefined';
     },
-    registerTool: function(tool) {
+
+    registerTool(tool) {
         this.tools[tool.id] = tool;
     },
-    useTool: function(toolId) {
+
+    useTool(toolId) {
         this.lastToolUsedId = toolId;
         this.tools[toolId].use();
     },
-    tryUseTool: function(toolId) {
+
+    tryUseTool(toolId) {
         if (!this.hasTool(toolId)) {
             return false;
         }
         this.useTool(toolId);
         return true;
     },
-    newDebugProcess: function(toolId) {
+
+    newDebugProcess(toolId) {
         if (!this.hasTool(toolId)) {
             return null;
         }
         return this.tools[toolId].getDebugProcess();
     }
+
 };

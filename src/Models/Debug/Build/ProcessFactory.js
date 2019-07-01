c3df8498fe79aeec591f9f06e7671d9954a3ce07
Nicholas Toby <cheesasaurus@gmail.com>
2019-06-30 23:33:02 -0500

    Factory stuff for PhaseAttempt

--- src/Models/Debug/Build/ProcessFactory.js
+++ src/Models/Debug/Build/ProcessFactory.js
@@ -1,13 +1,39 @@
 import { DebugProcess } from '/twcheese/src/Models/Debug/DebugProcess.js';
+import { PhaseFactory } from '/twcheese/src/Models/Debug/Build/PhaseFactory.js';
+import { BugReporter } from '/twcheese/src/Models/Debug/BugReporter.js';
+import { PhaseReport } from '/twcheese/src/Models/Debug/PhaseReport.js';
 
 class ProcessFactory {
 
-    create(cfg, actions) {
-        actions = actions || {};
+    constructor(actions) {
+        this.phaseFactory = new PhaseFactory(actions);
+    }
 
-        // todo: implement
+    create(cfg, finishWithReport) {
+        let process = new DebugProcess();
 
-        return new DebugProcess();
+        for (let phaseCfg of cfg.phases) {
+            process.enqueuePhase(this.createPhase(phaseCfg));
+        }
+
+        if (finishWithReport) {
+            let bugReporter = new BugReporter(debugProcess);
+            process.enqueuePhase(PhaseReport.create(bugReporter));
+        }
+
+        return process;
+    }
+
+    createPhase(cfg) {
+        let phase = this.phaseFactory.create(cfg);
+
+        if (cfg.type === 'PhaseAttempt' && cfg.success) {
+            for (let phaseCfg of cfg.success) {
+                phase.addSuccessFollowUp(this.createPhase(phaseCfg));
+            }
+        }
+
+        return phase;
     }
 
 };

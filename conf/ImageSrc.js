8dae25d99c6716366115f00766812527c31cf283
Nicholas Toby <cheesasaurus@gmail.com>
2019-07-01 14:18:59 -0500

    BRE: add missing jquery-ui assets

--- conf/ImageSrc.js
+++ conf/ImageSrc.js
@@ -14,7 +14,14 @@ let ImageSrc = {
     calendar: self + 'calendar.png',
     sidebarMain: self + 'sidebar/gear.png',
     sidebarBug: self + 'sidebar/bug.png',
-    sidebarGithub: self + 'sidebar/github.png'
+    sidebarGithub: self + 'sidebar/github.png',
+    jq: {
+        blue: self + 'jquery/ui-icons_2e83ff_256x240.png',
+        black: self + 'jquery/ui-icons_222222_256x240.png',
+        darkGrey: self + 'jquery/ui-icons_454545_256x240.png',
+        grey: self + 'jquery/ui-icons_888888_256x240.png',
+        red: self + 'jquery/ui-icons_cd0a0a_256x240.png'
+    }
 };
 
 export { ImageSrc };

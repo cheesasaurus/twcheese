7870b8b1d080d83b2871cae8aabfffe82e5722d3
Nicholas Toby <cheesasaurus@gmail.com>
2019-07-01 02:22:08 -0500

    use yaml + factory for OverviewHauls Default process

--- src/ToolDebug/OverviewHauls/Default.js
+++ src/ToolDebug/OverviewHauls/Default.js
@@ -1,16 +1,7 @@
-import { DebugProcess } from '/twcheese/src/Models/Debug/DebugProcess.js';
-import { BugReporter } from '/twcheese/src/Models/Debug/BugReporter.js';
-import { PhaseQuestion } from '/twcheese/src/Models/Debug/PhaseQuestion.js';
-import { QuestionFreeForm } from '/twcheese/src/Models/Debug/QuestionFreeForm.js';
-import { PhaseReport } from '/twcheese/src/Models/Debug/PhaseReport.js';
+import { ProcessFactory } from '/twcheese/src/Models/Debug/Build/ProcessFactory.js';
+import { processCfg } from '/twcheese/dist/tool/cfg/debug/OverviewHauls/Default.js';
 
-let debugProcess = DebugProcess.create('Tool: OverviewHauls');
-let bugReporter = new BugReporter(debugProcess);
-
-debugProcess
-    .enqueuePhase(PhaseQuestion.create('Entry')
-        .addQuestion(QuestionFreeForm.create(`What's broken?`, 'e.g. "it freezes when loading hauls"', 10))
-    )
-    .enqueuePhase(PhaseReport.create(bugReporter));
+let pf = new ProcessFactory({});
+let debugProcess = pf.create(processCfg, true);
 
 export { debugProcess };

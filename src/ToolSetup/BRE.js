b07a76ac44eee2dbf9d608a6410dacd96f44a4df
Nicholas Toby <cheesasaurus@gmail.com>
2019-07-01 11:14:48 -0500

    BRE: getCookie declare vars

--- src/ToolSetup/BRE.js
+++ src/ToolSetup/BRE.js
@@ -3932,10 +3932,10 @@ function twcheese_calculateRaidUnits(resources, haulBonus) {
 
 function getCookie(c_name) {
     if (document.cookie.length > 0) {
-        c_start = document.cookie.indexOf(c_name + "=");
+        var c_start = document.cookie.indexOf(c_name + "=");
         if (c_start != -1) {
             c_start = c_start + c_name.length + 1;
-            c_end = document.cookie.indexOf(";", c_start);
+            var c_end = document.cookie.indexOf(";", c_start);
             if (c_end == -1) c_end = document.cookie.length;
             return unescape(document.cookie.substring(c_start, c_end));
         }

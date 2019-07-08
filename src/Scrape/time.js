21899913cd2d03aee1bcec27b596168dcd492c3b
Nicholas Toby <cheesasaurus@gmail.com>
2019-07-08 14:31:02 -0500

    parseArrival: handle portuguese

--- src/Scrape/time.js
+++ src/Scrape/time.js
@@ -3,26 +3,31 @@ import { TwCheeseDate } from '/twcheese/src/Models/TwCheeseDate.js';
 // note: some worlds have milliseconds disabled
 
 /**
- * @param {string} text formatted the way tw does it. e.g. "Jun 12, 2019  15:36:23:000"
+ * @param {string} text formatted the way tw does it
  * @param {string} market two char market code. e.g. "en"
  * @return {TwCheeseDate}
  */
 function parseArrival(text, market) {    
     switch (market) {
-        case 'pt': return parseArrivalPortuguese();            
+        case 'pt': return parseArrivalPortuguese(text);            
     }
     return parseArrivalEnglish(text);
 };
 
 function parseArrivalEnglish(text) {
+    // e.g. "Jun 12, 2019  15:36:23:000"
     let expr = /(\D{3}) (\d{1,2}), (\d{4})  (\d{2}):(\d{2}):(\d{2}):?(\d{3})?/;
     let [, monthName, day, year, hours, minutes, seconds, millis] = text.match(expr);    
     let month = TwCheeseDate.monthNumber(monthName);
     return TwCheeseDate.newServerDate(year, month, day, hours, minutes, seconds, millis || 0);
 }
 
-function parseArrivalPortuguese() {
-    // todo
+function parseArrivalPortuguese(text) {
+    // e.g. "09/jul/2019 (20:03:15):895"
+    let expr = /(\d+)\/(\D+)\/(\d+) \((\d+):(\d+):(\d+)\):?(\d+)?/;
+    let [, day, monthName, year, hours, minutes, seconds, millis] = text.match(expr);
+    let month = TwCheeseDate.monthNumber(monthName);
+    return TwCheeseDate.newServerDate(year, month, day, hours, minutes, seconds, millis || 0);
 }
 
 
--- test/unit/Scrape/time/parseArrival.test.js
+++ test/unit/Scrape/time/parseArrival.test.js
@@ -20,7 +20,7 @@ function assertServerTime(expected, actual) {
 describe('parseArrival', function() {
 
     it('should handle en format', function() {
-        let actual = parseArrival('Jul 08, 2019  19:36:07:906');
+        let actual = parseArrival('Jul 08, 2019  19:36:07:906', 'en');
         let expected = {
             year: 2019,
             month: 6,
@@ -33,8 +33,18 @@ describe('parseArrival', function() {
         assertServerTime(expected, actual);
     });
 
-    xit('should handle pt format', function() {
-        // todo
+    it('should handle pt format', function() {
+        let actual = parseArrival('09/jul/2019 (20:03:15):895', 'pt');
+        let expected = {
+            year: 2019,
+            month: 6,
+            date: 9,
+            hours: 20,
+            minutes: 3,
+            seconds: 15,
+            millis: 895
+        };
+        assertServerTime(expected, actual);
     });
 
 });

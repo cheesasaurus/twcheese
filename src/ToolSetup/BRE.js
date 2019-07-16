f4317619ec29392fdc97556c1944b171f5585e36
Nicholas Toby <cheesasaurus@gmail.com>
2019-07-16 04:39:21 -0500

    BRE: extract ReportSelectorWidget

--- src/ToolSetup/BRE.js
+++ src/ToolSetup/BRE.js
@@ -1,7 +1,6 @@
 /* global $, game_data */
 import { initCss } from '/twcheese/src/Util/UI.js';
 import { ImageSrc } from '/twcheese/conf/ImageSrc.js';
-import { BattleReportCondensed } from '/twcheese/src/Models/BattleReportCondensed.js';
 import { ReportRenamer } from '/twcheese/src/Models/ReportRenamer.js';
 import { BattleReportScraper } from '/twcheese/src/Scrape/BattleReportScraper.js';
 import { BattleReportCondensedScraper } from '/twcheese/src/Scrape/BattleReportCondensedScraper.js';
@@ -9,6 +8,7 @@ import { enhanceBattleReport } from '/twcheese/src/Transform/enhanceBattleReport
 import { ReportToolsWidget } from '/twcheese/src/Widget/ReportToolsWidget.js';
 import { ReportListWidget } from '/twcheese/src/Widget/ReportsFolder/ReportListWidget.js';
 import { ReportSelector } from '/twcheese/src/Widget/ReportsFolder/ReportSelector.js';
+import { ReportSelectorWidget } from '/twcheese/src/Widget/ReportsFolder/ReportSelectorWidget.js';
 import { userConfig, ensureRemoteConfigsUpdated } from '/twcheese/src/Util/Config.js';
 import { requestDocument, gameUrl } from '/twcheese/src/Util/Network.js';
 import { ProcessFactory } from '/twcheese/src/Models/Debug/Build/ProcessFactory.js';
@@ -433,6 +433,10 @@ function twcheese_BattleReportsFolderEnhancer(gameDoc, renamer) {
     let reportListWidget = new ReportListWidget(this.reports);
     reportListWidget.appendTo(reportsFolder);
 
+    /*==== reports selector bar ====*/
+    let reportSelector = new ReportSelector(reportListWidget);
+    (new ReportSelectorWidget(reportSelector)).appendTo(reportsFolder);
+
 
     /**
      *	note: changed from a loop to recursive method in 2.2 to allow redrawing of progress in IE via setTimeout method
@@ -507,198 +511,7 @@ function twcheese_BattleReportsFolderEnhancer(gameDoc, renamer) {
             }
         }
     };
-
-
-    /*==== reports selector bar ====*/
-    let reportSelector = new ReportSelector(reportListWidget);
-
-    var reportsSelectorBar = document.createElement('div');
-    reportsFolder.appendChild(reportsSelectorBar);
-    reportsSelectorBar.id = 'twcheese_reportsSelectorBar';
-    reportsSelectorBar.style.borderStyle = 'solid';
-    reportsSelectorBar.style.borderWidth = '1px';
-
-    /*==== label ====*/
-    var reportsSelectorBarLabel = document.createElement('div');
-    reportsSelectorBar.appendChild(reportsSelectorBarLabel);
-    reportsSelectorBarLabel.style.backgroundColor = 'rgb(193, 162, 100) !important';
-    reportsSelectorBarLabel.style.backgroundImage = 'linear-gradient(rgb(229,194,126), rgb(193,162,100))';
-    reportsSelectorBarLabel.style.backgroundRepeat = 'repeat-x';
-    reportsSelectorBarLabel.style.fontSize = '9pt';
-    reportsSelectorBarLabel.style.fontWeight = '700';
-    reportsSelectorBarLabel.innerHTML = 'SELECT';
-    reportsSelectorBarLabel.style.height = '18px';
-    reportsSelectorBarLabel.style.paddingLeft = '3px';
-
-    /*==== clicky table ====*/
-    var selectorClickyTable = document.createElement('table');
-    reportsSelectorBar.appendChild(selectorClickyTable);
-    selectorClickyTable.className = 'vis';
-    selectorClickyTable.insertRow(-1);
-
-    let imgHtml = src => `<img style="display:block; margin-left:auto; margin-right:auto" src="${src}"/>`;
-
-    let clickyOptions = new Map([
-        ['all', {
-            click: () => reportSelector.selectAll(),
-            html: 'all'
-        }],
-        ['none', {
-            click: () => reportSelector.selectNone(),
-            html: 'none'
-        }],
-        ['new', {
-            click: () => reportSelector.selectNew(),
-            html: 'new'
-        }],
-        ['old', {
-            click: () => reportSelector.selectOld(),
-            html: 'old'
-        }],
-        ['dotGreen', {
-            click: () => reportSelector.selectDotColor('green'),
-            html: imgHtml('graphic/dots/green.png')
-        }],
-        ['dotYellow', {
-            click: () => reportSelector.selectDotColor('yellow'),
-            html: imgHtml('graphic/dots/yellow.png')
-        }],
-        ['dotRed', {
-            click: () => reportSelector.selectDotColor('red'),
-            html: imgHtml('graphic/dots/red.png')
-        }],
-        ['dotBlue', {
-            click: () => reportSelector.selectDotColor('blue'),
-            html: imgHtml('graphic/dots/blue.png')
-        }],
-        ['forwarded', {
-            click: () => reportSelector.selectForwarded(),
-            html: imgHtml('graphic/forwarded.png')
-        }],
-        ['haulPartial', {
-            click: () => reportSelector.selectLoot(BattleReportCondensed.HAUL_STATUS_PARTIAL),
-            html: imgHtml('graphic/max_loot/0.png')
-        }],
-        ['haulFull', {
-            click: () => reportSelector.selectLoot(BattleReportCondensed.HAUL_STATUS_FULL),
-            html: imgHtml('graphic/max_loot/1.png')
-        }],
-        ['feint', {
-            click: () => reportSelector.selectFeint(),
-            html: imgHtml('graphic/dots/grey.png'),
-            tooltip: 'feint'
-        }],
-        ['deadNoble', {
-            click: () => reportSelector.selectDeadNoble(),
-            html: imgHtml(ImageSrc.troopIcon('priest')),
-            tooltip: 'dead noble'
-        }],
-        ['loyalty', {
-            click: () => reportSelector.selectLoyalty(),
-            html: '<span style="display:block; margin-left:auto; margin-right:auto" class="icon ally lead"/>',
-            tooltip: 'loyalty change'
-        }],
-        ['cleared', {
-            click: () => reportSelector.selectCleared(),
-            html: 'defenseless'
-        }]
-    ]);
-
-    for (let [descriptor, option] of clickyOptions) {
-        let optionEl = $(`<a href="#">${option.html}</a>`)[0];
-        if (option.tooltip) {
-            optionEl.title = option.tooltip;
-        }
-        optionEl.addEventListener('click', function(e) {
-            e.preventDefault();
-            option.click();
-        });
-
-        let cell = selectorClickyTable.rows[0].insertCell(-1);
-        cell.style.width = '25px';
-        cell.style.textAlign = 'center';        
-        cell.appendChild(optionEl);
-    }
-
-    /*==== input table ====*/
-    var selectorInputTable = document.createElement('table');
-    reportsSelectorBar.appendChild(selectorInputTable);
-    selectorInputTable.className = 'vis';
-    selectorInputTable.insertRow(-1);
-
-    let inputOptions = [
-        {
-            hintInput: 'contains text',
-            hintButton: 'select text',
-            use: (text) => reportSelector.selectText(text),
-            sprite: [-140, 0]
-        },
-        {
-            hintInput: 'attacker',
-            hintButton: 'select attacking player',
-            use: (attackerName) => reportSelector.selectAttacker(attackerName),
-            sprite: [-80, 0]
-        },
-        {
-            hintInput: 'defender',
-            hintButton: 'select defending player',
-            use: (defenderName) => reportSelector.selectDefender(defenderName),
-            sprite: [-80, 0]
-        },
-        {
-            hintInput: 'origin',
-            hintButton: 'select attacking village',
-            placeholder: 'x|y',
-            use: (coords) => reportSelector.selectAttackerVillage(coords),
-            sprite: [-120, 0]
-        },
-        {
-            hintInput: 'target',
-            hintButton: 'select defending village',
-            placeholder: 'x|y',
-            use: (coords) => reportSelector.selectDefenderVillage(coords),
-            sprite: [-120, 0]
-        }
-    ];
-
-    for (let option of inputOptions) {
-        let input = document.createElement('input');
-        input.type = 'text';
-        input.size = 10;
-        input.value = option.hintInput;
-        input.placeholder = option.placeholder || '';
-        let alreadyCleared = false;
-        input.addEventListener('mousedown', function() {
-            if (alreadyCleared) {
-                return;
-            }
-            this.value = '';
-            alreadyCleared = true;
-        });
-
-        let $button = $(`<a href="#" title="${option.hintButton}"></a>`)
-            .on('click', function(e) {
-                e.preventDefault();
-                option.use(input.value);
-            });
-
-        let $buttonIcon = $('<span>&nbsp;</span>')
-            .css({
-                display: 'inline-block',
-                background: `url(graphic/bbcodes/bbcodes.png) no-repeat ${option.sprite[0]}px ${option.sprite[1]}px`,
-                paddingLeft: 0,
-                paddingBottom: 0,
-                margin: 3,
-                width: 20,
-                height: 20
-            });
-
-        $button.append($buttonIcon);
-
-        let cell = selectorInputTable.rows[0].insertCell(-1);
-        cell.appendChild(input);
-        cell.appendChild($button[0]);
-    }
+    
 
     /*==== mass rename table ===*/
 

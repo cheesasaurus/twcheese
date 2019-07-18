84d9b89e3d241f0a001952771572cb301cb27f22
Nicholas Toby <cheesasaurus@gmail.com>
2019-07-17 21:15:25 -0500

    BRE: extract spawnLegacyHelpButton to module

--- src/ToolSetup/BRE.js
+++ src/ToolSetup/BRE.js
@@ -1,10 +1,10 @@
 /* global $, game_data */
 import { initCss } from '/twcheese/src/Util/UI.js';
-import { ImageSrc } from '/twcheese/conf/ImageSrc.js';
 import { ReportRenamer } from '/twcheese/src/Models/ReportRenamer.js';
 import { BattleReportScraper } from '/twcheese/src/Scrape/BattleReportScraper.js';
 import { BattleReportCondensedScraper } from '/twcheese/src/Scrape/BattleReportCondensedScraper.js';
 import { enhanceBattleReport } from '/twcheese/src/Transform/enhanceBattleReport.js';
+import { spawnLegacyHelpButton } from '/twcheese/src/Transform/spawnLegacyHelpButton.js';
 import { ReportToolsWidget } from '/twcheese/src/Widget/ReportToolsWidget.js';
 import { ReportsFolderWidget } from '/twcheese/src/Widget/ReportsFolder/ReportsFolderWidget.js';
 import { userConfig, ensureRemoteConfigsUpdated } from '/twcheese/src/Util/Config.js';
@@ -19,81 +19,6 @@ initCss(`
         margin-bottom: 30px;
     }
 `);
-
-// language /////////////////////////////////////////////////////////////////////
-
-var language = { "twcheese": {} };
-switch (game_data.market) {
-    default:
-        /*==== tribalwars.net, tribalwars.us, tribalwars.co.uk, beta.tribalwars.net ====*/
-        language['twcheese']['Help'] = 'Help';
-        break;
-
-    case 'cz':
-        /*==== divokekmeny.cz/ ====*/
-        language['twcheese']['Help'] = 'Pomoc';
-        break;
-
-    case 'se':
-        language['twcheese']['Help'] = 'Hjälp';
-        break;
-
-    /*==== fyletikesmaxes.gr/ ====*/
-    case 'gr':
-        language['twcheese']['Help'] = 'Βοήθεια';
-        break;
-
-    /* Norwegian */
-    case 'no':
-        language['twcheese']['Help'] = 'Hjelp';
-        break;
-                        
-}
-
-
-/*==== page modifier functions ====*/
-
-
-/**
- *	@param	text:String	text to be displayed inside the button
- *	@param	address:String	(optional) if included, causes the button to open a new window when clicked, directing the page to the specified address
- */
-function createFooterButton(text, address) {
-    var twcheese_menu = document.createElement('div');
-    twcheese_menu.style.textAlign = 'center';
-
-    var twcheese_menu_text = document.createElement('p');
-    twcheese_menu_text.style.fontSize = '9pt';
-    twcheese_menu_text.innerHTML = text;
-    /*twcheese_menu_text.style.fontFamily = '"Comic Sans MS", cursive, sans-serif';*/
-    twcheese_menu_text.style.fontWeight = '700';
-    twcheese_menu_text.style.marginTop = '3px';
-    twcheese_menu_text.style.color = '#422301';
-    twcheese_menu.appendChild(twcheese_menu_text);
-
-    twcheese_menu.style.background = `url("${ImageSrc.legacy.helpBackground}")`;
-    twcheese_menu.style.height = '22px';
-    twcheese_menu.style.width = '49px';
-    twcheese_menu.style.display = 'block';
-    twcheese_menu.style.position = 'fixed';
-    twcheese_menu.style.left = '100%';
-    twcheese_menu.style.top = '100%';
-    twcheese_menu.style.marginTop = '-24px';
-    twcheese_menu.style.marginLeft = '-52px';
-    twcheese_menu.style.zIndex = '99999999999';
-
-    twcheese_menu.onmouseover = function () { this.style.background = `url("${ImageSrc.legacy.helpBackgroundBright}")` };
-    twcheese_menu.onmouseout = function () { this.style.background = `url("${ImageSrc.legacy.helpBackground}")` };
-
-    if (address) {
-        twcheese_menu.style.cursor = 'pointer';
-        twcheese_menu.onclick = function () { window.open(address, 'twcheese_menu_window') };
-    }
-    else
-        twcheese_menu.style.cursor = 'default';
-
-    return document.body.appendChild(twcheese_menu);
-};
 
 
 /*==== main ====*/
@@ -138,7 +63,7 @@ function initBRE() {
     narcismDiv.style.fontSize = '10px';
 
     /*==== help ====*/
-    createFooterButton(language['twcheese']['Help'], 'https://forum.tribalwars.net/index.php?threads/256225/');
+    spawnLegacyHelpButton('https://forum.tribalwars.net/index.php?threads/256225/');
 }
 
 

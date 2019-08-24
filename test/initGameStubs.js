f7d0a47ff700b70639951abd8df1c4f7a1d1e6b1
Nicholas Toby <cheesasaurus@gmail.com>
2019-08-24 11:41:54 -0500

    troop config for tests

--- test/initGameStubs.js
+++ test/initGameStubs.js
@@ -8,7 +8,8 @@ window.image_base = 'https://dsen.innogamescdn.com/asset/5cfabaf2/graphic/';
 window.server_utc_diff = 3600;
 
 window.game_data = {
-    market: 'en'
+    market: 'en',
+    units: ['spear', 'sword', 'axe', 'spy', 'light', 'heavy', 'ram', 'catapult', 'knight', 'snob', 'militia']
 };
 
 window.Timing = {

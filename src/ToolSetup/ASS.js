c60771dc09af9abf8a281d6a589f207a140caab9
Nicholas Toby <cheesasaurus@gmail.com>
2019-08-25 03:07:53 -0500

    ASS: redirect to scavenge screen if not there

--- src/ToolSetup/ASS.js
+++ src/ToolSetup/ASS.js
@@ -1,4 +1,5 @@
 import { userConfig, ensureRemoteConfigsUpdated } from '/twcheese/src/Util/Config.js';
+import { suggestRedirect } from '/twcheese/src/Prompt/suggestRedirect.js';
 import { ProcessFactory } from '/twcheese/src/Models/Debug/Build/ProcessFactory.js';
 import { processCfg as debugCfgDefault } from '/twcheese/dist/tool/cfg/debug/BRE/Default.js';
 
@@ -11,7 +12,31 @@ async function useTool() {
         initialized = true;
     }
 
+    if (!atScavengeScreen()) {
+        suggestRedirectToScavengeScreen();
+        return;
+    }
+
     // todo
+    
+}
+
+
+function atScavengeScreen() {
+    let here = document.location.href;
+    return here.includes('screen=place') && here.includes('mode=scavenge');
+}
+
+
+function suggestRedirectToScavengeScreen() {
+    suggestRedirect({
+        message: 'To use this, you must be at the scavenging screen.',
+        screen: 'place',
+        uriParams: {
+            mode: 'scavenge'
+        },
+        skippableId: 'Tool:ASS'
+    });
 }
 
 

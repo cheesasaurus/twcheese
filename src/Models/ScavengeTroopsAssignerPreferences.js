d4dba0149cdc49fd77a3239a161dadf0bf14cf89
Nicholas Toby <cheesasaurus@gmail.com>
2019-08-26 23:12:05 -0500

    extract ScavengeTroopsAssignerPreferences

---
+++ src/Models/ScavengeTroopsAssignerPreferences.js
@@ -0,0 +1,52 @@
+import { TroopCounts } from '/twcheese/src/Models/Troops.js';
+
+
+class ScavengeTroopsAssignerPreferences {
+
+    constructor(sendableTroopTypes) {
+        this._sendableTroopTypes = sendableTroopTypes;
+
+        this.mode = ScavengeTroopsAssignerPreferences.MODE_SANE_PERSON;
+        this.targetDurationSeconds = 2 * 3600;
+        this.initTroops();
+        this.troopOrder = [
+            ['axe', 'light', 'marcher'], // first chunk (sent together)
+            ['spear', 'sword', 'archer'], // second chunk (sent together)
+            ['heavy'] // third chunk
+        ];
+    }
+
+    initTroops() {
+        this.troops = {};
+        for (let troopType of this._sendableTroopTypes) {
+            this.troops[troopType] = {
+                maySend: true,
+                reserved: 0
+            };
+        }
+    }
+
+    /**
+     * @return {string[]}
+     */
+    getAllowedTroopTypes() {
+        return this._sendableTroopTypes.filter(troopType => this.troops[troopType].maySend);
+    }
+
+    /**
+     * @return TroopCounts
+     */
+    getReservedTroopCounts() {
+        let troopCounts = new TroopCounts();
+        for (let troopType of this._sendableTroopTypes) {
+            troopCounts[troopType] = this.troops[troopType].reserved;
+        }
+        return troopCounts;
+    }
+
+}
+
+ScavengeTroopsAssignerPreferences.MODE_SANE_PERSON = 'sane_person';
+ScavengeTroopsAssignerPreferences.MODE_ADDICT = 'addict';
+
+export { ScavengeTroopsAssignerPreferences };
